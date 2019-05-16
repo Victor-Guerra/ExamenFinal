5756887d225f473559bb7c8a7dc8698d1b25c0be
Victor_Guerra <a01568075@itesm.mx>
2019-05-16 12:06:16 -0600

    Reactivo 3

---
+++ Reactivo3/Clases/NombreInvalidoException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactivo3.Clases
+{
+
+    class NombreInvalidoException : Exception
+    {
+        public const string Mensaje = "Nombre/Apellido no valido.";
+        public NombreInvalidoException() { }
+        public NombreInvalidoException(string message) : base(message)
+        {
+        }
+        public NombreInvalidoException(string message, Exception inner) : base(message, inner) { }
+    }
+
+}
---
+++ Reactivo3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactivo3.Clases;
+
+
+namespace Reactivo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            try
+            {
+
+                    Console.WriteLine("De su nombre:");
+                    nombre = Console.ReadLine().Trim(new char[] { '\n', '\r' });
+
+                    Console.WriteLine("De su apellido:");
+                    apellido = Console.ReadLine().Trim(new char[] { '\n', '\r' });
+
+                    if (apellido.Length <= 2 || nombre.Length <= 2)
+                    {
+                        throw new NombreInvalidoException();
+                    }
+            }
+            catch (NombreInvalidoException)
+            {
+                Console.WriteLine(NombreInvalidoException.Mensaje);
+            }
+        }
+    }
+}
